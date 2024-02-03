0edc9b51ec55a1f5a556dd6ff5ca081185d8685c
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2024-02-04 00:44:46 +0200

    ADD: company-form

---
+++ src/app/components/company-form-modal.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import React from 'react';
+import CompanyForm, { CompanyFormProps } from '@/app/components/company-form';
+import Modal, { ModalProps } from '@/app/components/modal';
+
+export interface CompanyFormModalProps extends ModalProps {
+    onSubmit: CompanyFormProps['onSubmit'];
+}
+
+export default function CompanyFormModal({
+    onClose,
+    onSubmit,
+    ...rest
+}: CompanyFormModalProps) {
+    return (
+        <Modal {...rest} onClose={onClose}>
+            <CompanyForm onSubmit={() => onClose()} />
+        </Modal>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
-import StatusLabel, { Status } from './components/status-label';
+import AddCompanyButton from './components/add-company-button';
+// import StatusLabel, { Status } from './components/status-label';
 
 export default function Home() {
     return (
         <main>
-            <h1 className="text-xl">Home page</h1>
-            <StatusLabel status={Status.Active}>Active</StatusLabel>
-            <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-            <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-            <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+            <AddCompanyButton>Add company</AddCompanyButton>
+            <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
+            {/* <StatusLabel status={Status.Active} disabled={false}>
+                Active
+            </StatusLabel>
+            <StatusLabel status={Status.NotActive} disabled={true}>
+                Not Active
+            </StatusLabel>
+            <StatusLabel status={Status.Pending} disabled={false}>
+                Pending
+            </StatusLabel>
+            <StatusLabel status={Status.Suspended} disabled={false}>
+                Suspended
+            </StatusLabel> */}
         </main>
     );
 }
